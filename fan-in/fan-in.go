978aeb783741b196b451770f4611bc056bb71546
Joan López de la Franca Beltran <5459617+joanlopez@users.noreply.github.com>
2021-12-16 11:25:36 +0100

    gofmt

--- fan-in/fan-in.go
+++ fan-in/fan-in.go
@@ -12,31 +12,31 @@ func main() {
 	ch := fanin(ch1, ch2)
 	go func() {
 		for v := range ch {
-            println(v)
-        }
-    }()
+			println(v)
+		}
+	}()
 	time.Sleep(2 * time.Second)
 }
 
 func fanin(chs ...<-chan string) <-chan string {
-    c := make(chan string)
-    for _, ch := range chs {
-        go func(ch <-chan string) {
-            for {
-                c <- <-ch
-            }
-        }(ch)
-    }
-    return c
+	c := make(chan string)
+	for _, ch := range chs {
+		go func(ch <-chan string) {
+			for {
+				c <- <-ch
+			}
+		}(ch)
+	}
+	return c
 }
 
-func dataSource(data string) <- chan string {
-    ch := make(chan string)
-    go func() {
-        for {
+func dataSource(data string) <-chan string {
+	ch := make(chan string)
+	go func() {
+		for {
 			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
 			ch <- data
-        }
-    }()
-    return ch
+		}
+	}()
+	return ch
 }
